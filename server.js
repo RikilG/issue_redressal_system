5b4550085f2dd54989e37260268688b5b5ea26b8
Raj Kashyap <rajkashyapmallela@gmail.com>
2019-03-02 23:07:56 +0530

    added register db

--- server.js
+++ server.js
@@ -2,11 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const mongoose = require('mongoose');
-
+const mongo = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
+mongo.connect('mongodb://raj:raj1@cluster0-shard-00-00-ojo88.gcp.mongodb.net:27017,cluster0-shard-00-01-ojo88.gcp.mongodb.net:27017,cluster0-shard-00-02-ojo88.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',{useNewUrlParser:true},function(err){
+if(err)
+  console.log(err);
+  else
+console.log("connected");
+});
+
+var schema1=new Schema({ firstname:String,
+                        lastname:String,
+                        email:String,
+                        password:String,
+                        address1:String,
+                        address2:String,
+                        city:String,
+                        state:String,
+                        pincode:Number
+                        });
+var customer=mongo.model('customer',schema);
+
+var schema2=new Schema({})
 //serve react static files.
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
@@ -32,6 +51,10 @@ app.post('/login',(req,res) => {
         });
     }
 })
+app.post('/register',function(req,res){
+    var newcustm=new customer(req.body.json);
+    newcustm.save();
+});
 
 app.post('/feed',(req,res) => {
     res.json({
