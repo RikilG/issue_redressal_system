05892b2bf8834d3e96147799e9ed410acabb0e1c
SrihariBits <47473205+SrihariBits@users.noreply.github.com>
2019-10-29 20:15:33 +0530

    Added a missing requirement

--- client/src/Components/Login/FormLogin.js
+++ client/src/Components/Login/FormLogin.js
@@ -27,44 +27,44 @@ class FormLogin extends Component {
   postRequest = () => {
     fetch("/login", {
       method: "post",
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         email: this.state.email,
         password: this.state.password,
       })
     })
-    .then(res => res.json())
-    .then(data => {
-      if(data.isAdmin) {
-        this.props.setSigninStatus(true,this.state.email);
-        this.props.setView("AdminHome");
-      }
-      else if(data.validUser) {
-        this.props.setSigninStatus(true,this.state.email);
-        this.props.setView("Feed");
-      }
-      else{
-        alert("Invalid credentials");
-      }
-    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.isAdmin) {
+          this.props.setSigninStatus(true, this.state.email);
+          this.props.setView("AdminHome");
+        }
+        else if (data.validUser) {
+          this.props.setSigninStatus(true, this.state.email);
+          this.props.setView("Feed");
+        }
+        else {
+          alert("Invalid credentials:(");
+        }
+      })
   }
 
   render() {
     return (
-        <div className="formlogin">
-           <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control className="tvEmail" type="email" placeholder="Enter email" onChange={this.onEmailChange} />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control className="tvPassword" type="password" placeholder="Password" onChange={this.onPasswordChange} />
-                </Form.Group>
-                <Button id="btnLogin" variant="primary" onClick={this.postRequest} >Login</Button><br /><br />
-                <Button id="btnRegister" variant="secondary" onClick={this.onClickRegister} >Not a user? Register here</Button>
-            </Form> 
-        </div>
+      <div className="formlogin">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control className="tvEmail" type="email" placeholder="Enter email" onChange={this.onEmailChange} />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control className="tvPassword" type="password" placeholder="Password" onChange={this.onPasswordChange} />
+          </Form.Group>
+          <Button id="btnLogin" variant="primary" onClick={this.postRequest} >Login</Button><br /><br />
+          <Button id="btnRegister" variant="secondary" onClick={this.onClickRegister} >Not a user? Register here</Button>
+        </Form>
+      </div>
     );
   }
 }
